6d5c567b01d3573c11a1e644f4839e39e7dbefd2
Aryan Mehra <aryanmehra998@gmail.com>
2026-06-28 17:43:05 +0530

    Initial commit: Weather App

---
+++ Readme.md
@@ -0,0 +1,43 @@
+# 🌤️ Weather App
+
+A clean and responsive Weather App built using HTML, CSS and JavaScript. It fetches real-time weather information using the OpenWeather API.
+
+## 🚀 Features
+
+- Search weather by city
+- Live temperature
+- Humidity
+- Feels Like temperature
+- Current weather condition
+- Real-time date
+- Error handling for invalid cities
+- Responsive UI
+
+## 🛠️ Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+- OpenWeather API
+
+## 📸 Screenshot
+
+![Weather App](assets/screenshot.png)
+
+## 📦 Installation
+
+1. Clone the repository
+
+```bash
+git clone https://github.com/YourUsername/Weather-App.git
+```
+
+2. Open index.html in browser.
+
+## API
+
+OpenWeather API
+
+## Author
+
+Aryan Mehra
---
+++ script.js
@@ -0,0 +1,41 @@
+const apiKey = '40a692af3a062818c399e83433b64b27';
+
+
+async function getWeather(){
+    
+    const city = document.getElementById('search').value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    
+    let response = await fetch(url)
+    const data = await response.json()
+
+        if (!response.ok) {
+        document.getElementById("cityname").innerText = "City Not Found";
+        document.getElementById("temp").innerText = "--";
+        document.getElementById("wthr").innerText = "--";
+        document.getElementById("humidindex").innerText = "--";
+        document.getElementById("feelindex").innerText = "--";
+        return;
+        }
+    
+    document.getElementById('cityname').innerText = data.name;
+
+    document.getElementById("datetime").innerText = new Date().toDateString();
+
+    document.getElementById("temp").innerText = `${data.main.temp} °C`;
+
+    document.getElementById("wthr").innerText = data.weather[0].main
+
+    document.getElementById("humidindex").innerText = data.main.humidity
+
+    document.getElementById("feelindex").innerText = `${data.main.feels_like} °C`;
+
+
+
+    console.log(data)   
+}
+
+    
+
+document.getElementById('btn').addEventListener('click', getWeather);
+
